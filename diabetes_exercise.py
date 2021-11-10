7e94030dd8de38c533bb25f3e662b7f5bb6c6f9b
Caroline Wescott <caroline_wescott1@baylor.edu>
2021-11-10 16:37:56 -0600

    ML day 4

--- diabetes_exercise.py
+++ diabetes_exercise.py
@@ -6,24 +6,49 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn import datasets
 
+from sklearn.datasets import load_diabetes
+diabetes = load_diabetes()
 
-#how many sameples and How many features?
-
+#how many samples and How many features?
+print(diabetes.data.shape)
+# returns "(442, 10)" meaning 442 samples and 10 features
 
 
 # What does feature s6 represent?
-
-
-
+print(diabetes.DESCR)
 
 
 #print out the coefficient
+from sklearn.model_selection import train_test_split
 
+data_train, data_test, target_train, target_test = train_test_split(
+    diabetes.data, diabetes.target, random_state=11
+)
+
+mymodel = LinearRegression()
+
+mymodel.fit(X=data_train, y=target_train)
+
+coef = mymodel.coef_
+print(coef)
 
 
 #print out the intercept
+intercept = mymodel.intercept_
+print(intercept)
 
-
+# use predict to test your model
+predicted = mymodel.predict(data_test)
+expected = target_test
 
 # create a scatterplot with regression line
+plt.plot(expected, predicted, ".")
 
+
+x = np.linspace(0,330,100)
+print(x)
+y = x
+plt.plot(x,y)
+plt.show()
+
+# not an accurate model because the dots are so far away from the line
